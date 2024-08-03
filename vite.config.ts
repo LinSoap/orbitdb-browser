038e2dd133276dc918d4622c4c7022ae297a7f36
LinSoap <838819583@qq.com>
2024-08-03 23:50:13 +0800

    fix:add https support webtransport

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import fs from 'fs';
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
+
+    server: {
+    https: {
+      key: fs.readFileSync(path.resolve("/home/linsoap", 'localhost.key')),
+      cert: fs.readFileSync(path.resolve("/home/linsoap", 'localhost.crt')),
+    },
+    host: '0.0.0.0', // 确保服务器可以通过IP地址访问
+  },
   plugins: [react()],
 })
