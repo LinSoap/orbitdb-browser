e4ad01e7f84a355067d3fbf8bdea4029e4b5109c
LinSoap <838819583@qq.com>
2024-08-11 15:11:11 +0800

    fix:fix documentsEditItem update no immediately

--- src/components/common/form/DocumentsEditItem.tsx
+++ src/components/common/form/DocumentsEditItem.tsx
@@ -1,4 +1,4 @@
-import { Tag, TagLabel } from "@chakra-ui/react";
+import { Box, Tag, TagLabel } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import StyledInput from "../StyledInput";
 import { DocumentsValue } from "@orbitdb/core";
@@ -20,20 +20,14 @@ const DocumentsEditItem = ({
   const [editItem, setEditItem] = useState({ [key]: value });
 
   useEffect(() => {
-    return () => {
-      console.log("Component is being unmounted");
-      console.log(key, editItem[key]);
-      console.log("edit" + JSON.stringify(editItem));
-      updateNewValue();
-    };
-  }, []);
+    updateNewValue();
+  }, [editItem]);
 
   const updateNewValue = () => {
     setNewValue((prevValue) => {
       const updatedEntries = Object.entries(prevValue).map(([k, v], i) =>
         i === index ? [key, editItem[key]] : [k, v]
       );
-      // console.log(Object.fromEntries(updatedEntries));
       return Object.fromEntries(updatedEntries);
     });
   };
@@ -41,29 +35,30 @@ const DocumentsEditItem = ({
   const handleKeyChange = (newKey: string, newValue: string) => {
     setEditItem({ [newKey]: newValue });
     setKey(newKey);
-    console.log(editItem);
-    updateNewValue();
   };
 
   return (
     <Tag>
       {indexBy === key ? (
-        <TagLabel>* {key}</TagLabel>
+        <TagLabel>
+          {indexBy === key ? "*" + key : key}:{value}
+        </TagLabel>
       ) : (
-        <StyledInput
-          value={key}
-          w={"auto"}
-          onChange={(e) => handleKeyChange(e.target.value, editItem[key])}
-        />
+        <Box>
+          <StyledInput
+            value={key}
+            w={"auto"}
+            onChange={(e) => handleKeyChange(e.target.value, editItem[key])}
+          />
+
+          <StyledInput
+            value={editItem[key]}
+            w={"auto"}
+            onChange={(e) => setEditItem({ [key]: e.target.value })}
+          />
+        </Box>
       )}
       :
-      {
-        <StyledInput
-          value={editItem[key]}
-          w={"auto"}
-          onChange={(e) => setEditItem({ [key]: e.target.value })}
-        />
-      }
     </Tag>
   );
 };
